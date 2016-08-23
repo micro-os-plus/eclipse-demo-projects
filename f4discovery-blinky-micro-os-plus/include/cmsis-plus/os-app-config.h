fb780c316827d0f349b9a237e3e34424835d7f4a
Liviu Ionescu <ilg@livius.net>
2016-08-23 19:58:35 +0300

    f4: fix flash origin 0x08000000

--- f4discovery-blinky-micro-os-plus/include/cmsis-plus/os-app-config.h
+++ f4discovery-blinky-micro-os-plus/include/cmsis-plus/os-app-config.h
@@ -39,12 +39,8 @@
 
 // With 4 bits NVIC, there are 16 levels, 0 = highest, 15 = lowest
 
-#if 1
 // Disable all interrupts from 15 to 4, keep 3-2-1 enabled
 #define OS_INTEGER_RTOS_CRITICAL_SECTION_INTERRUPT_PRIORITY (4)
-#endif
-
-#if defined(__ARM_EABI__)
 
 #define OS_INTEGER_RTOS_MAIN_STACK_SIZE_BYTES               (3000)
 
@@ -53,34 +49,16 @@
 #define OS_EXCLUDE_RTOS_IDLE_SLEEP (1)
 #endif
 
-#endif /* defined(__ARM_EABI__) */
-
 #define OS_INCLUDE_RTOS_STATISTICS_THREAD_CONTEXT_SWITCHES  (1)
 #define OS_INCLUDE_RTOS_STATISTICS_THREAD_CPU_CYCLES        (1)
 
 // ----------------------------------------------------------------------------
 
-#if defined(USE_FREERTOS)
+#if 0
 
-// Request the inclusion of a custom implementations.
-#define OS_USE_RTOS_PORT_SCHEDULER                      (1)
-
-#if 1
-#define OS_USE_RTOS_PORT_TIMER                          (1)
-#define OS_USE_RTOS_PORT_SYSTICK_CLOCK_SLEEP_FOR        (1)
-#define OS_USE_RTOS_PORT_MUTEX                          (1)
-#define OS_USE_RTOS_PORT_SEMAPHORE                      (1)
-#define OS_USE_RTOS_PORT_MESSAGE_QUEUE                  (1)
-#define OS_USE_RTOS_PORT_EVENT_FLAGS                    (1)
-#endif
-
-#endif /* defined(USE_FREERTOS) */
-
-// ----------------------------------------------------------------------------
-
-#if defined(DEBUG)
-
+// Available trace options.
 #define OS_TRACE_RTOS_CLOCKS
+#define OS_TRACE_RTOS_LISTS_CLOCKS
 #define OS_TRACE_RTOS_CONDVAR
 #define OS_TRACE_RTOS_EVFLAGS
 #define OS_TRACE_RTOS_MEMPOOL
@@ -89,29 +67,22 @@
 #define OS_TRACE_RTOS_RTC_TICK
 #define OS_TRACE_RTOS_SCHEDULER
 #define OS_TRACE_RTOS_SEMAPHORE
+#define OS_TRACE_RTOS_SYSCLOCK_TICK
 #define OS_TRACE_RTOS_THREAD
+#define OS_TRACE_RTOS_THREAD_CONTEXT
 #define OS_TRACE_RTOS_THREAD_FLAGS
 #define OS_TRACE_RTOS_TIMER
+
+#define OS_TRACE_RTOS_LISTS
 
 #define OS_TRACE_LIBC_MALLOC
 #define OS_TRACE_LIBC_ATEXIT
 
-#if !defined(__ARM_EABI__) || defined(OS_USE_TRACE_SEGGER_RTT)
-#define OS_TRACE_RTOS_SYSCLOCK_TICK
-#define OS_TRACE_RTOS_LISTS
-#define OS_TRACE_RTOS_LISTS_CLOCKS
-#define OS_TRACE_RTOS_THREAD_CONTEXT
 #endif
 
-#else
+
 #define OS_TRACE_RTOS_THREAD
-#define OS_TRACE_RTOS_MQUEUE
-#define OS_TRACE_RTOS_MEMPOOL
-#define OS_TRACE_LIBC_MALLOC
-#endif
-
 #define OS_TRACE_RTOS_RTC_TICK
-//#define OS_TRACE_RTOS_SYSCLOCK_TICK
 
 // ----------------------------------------------------------------------------
 
